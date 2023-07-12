7268782cc45ba268c497f970f8b21c3aa97eea7c
Johan Fagerberg <johanringmann@gmail.com>
2023-07-12 21:57:47 +0200

    fix: remove reference to uninstalled package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-import svgr from "vite-plugin-svgr";
 import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
